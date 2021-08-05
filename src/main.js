a07f1af5c439ee3b16ef76ddcef466d643792065
Ana Ventura <anaventura1811@gmail.com>
2021-08-05 12:37:08 -0300

    Retornando JSON com status 200

--- src/main.js
+++ src/main.js
@@ -1,22 +1,31 @@
 import { createServer } from 'http';
+import { stringify } from 'querystring';
 
 const server = createServer((request, response) => {
   switch (request.url) {
     case '/status': {
-      response.writeHead(200);
-      response.write('Ok');
+      response.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      response.write(
+        JSON.stringify({
+          status: 'Ok',
+        })
+      );
       response.end();
-      return;
+      break;
     }
     default: {
-        response.writeHead(404, 'Server not found');
-				response.write('Not ok');
-				response.end();
-				return;
+      response.writeHead(404, 'Server not found');
+			response.write('Not ok');
+			response.end();
     }
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('server está ouvindo na porta http://localhost:8000')
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`server está ouvindo na porta http://${HOSTNAME}:${PORT}`)
 });
