64268442d4b78d4638abff776f98b51b8820f765
Ana Ventura <anaventura1811@gmail.com>
2021-08-05 12:21:04 -0300

    Diferentes tipos de resposta às requisições

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log('Oi');
+import { createServer } from 'http';
+
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case '/status': {
+      response.writeHead(200);
+      response.write('Ok');
+      response.end();
+      return;
+    }
+    default: {
+        response.writeHead(404, 'Server not found');
+				response.write('Not ok');
+				response.end();
+				return;
+    }
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('server está ouvindo na porta http://localhost:8000')
+});
